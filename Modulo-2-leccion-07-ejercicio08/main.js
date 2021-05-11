8f20a8c1c61b91c08f554720f7d3bead0d630c57
Lucia Cano <luciacanoras@gmail.com>
2021-05-11 19:01:28 +0200

    Add loop exercise 08 lesson 7 module 2

--- Modulo-2-leccion-07-ejercicio08/main.js
+++ Modulo-2-leccion-07-ejercicio08/main.js
@@ -4,6 +4,9 @@ const corp = document.querySelector(".js-container");
 function handlerButton() {
   corp.classList.toggle("backred");
 }
-classes[0].addEventListener("click", handlerButton);
-classes[1].addEventListener("click", handlerButton);
-classes[2].addEventListener("click", handlerButton);
+// classes[0].addEventListener("click", handlerButton);
+// classes[1].addEventListener("click", handlerButton);
+// classes[2].addEventListener("click", handlerButton);
+for (let i = 0; i < classes.length; i++) {
+  classes[i].addEventListener("click", handlerButton);
+}
