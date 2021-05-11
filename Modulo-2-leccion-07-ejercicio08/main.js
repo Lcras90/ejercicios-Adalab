18ac7d079985b6d7ddb1ae94ad81f343455097cd
Lucia Cano <luciacanoras@gmail.com>
2021-05-11 18:03:40 +0200

    Fix exercise 08 lesson 7 module 2

--- Modulo-2-leccion-07-ejercicio08/main.js
+++ Modulo-2-leccion-07-ejercicio08/main.js
@@ -1,12 +1,9 @@
 const classes = document.querySelectorAll("button");
 const corp = document.querySelector(".js-container");
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-const button3 = document.querySelector(".button3");
 
 function handlerButton() {
   corp.classList.toggle("backred");
 }
-button1.addEventListener("click", handlerButton);
-button2.addEventListener("click", handlerButton);
-button3.addEventListener("click", handlerButton);
+classes[0].addEventListener("click", handlerButton);
+classes[1].addEventListener("click", handlerButton);
+classes[2].addEventListener("click", handlerButton);
