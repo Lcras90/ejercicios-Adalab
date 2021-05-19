c905668256c53a73024e399873573888e839c4ed
Lucia Cano <luciacanoras@gmail.com>
2021-05-19 11:28:33 +0200

    Add middle module 2 lesson 12 exercise 06

---
+++ Modulo-2-leccion-12-ejercicio-06/main.js
@@ -0,0 +1,32 @@
+const list = document.querySelector(".js-list");
+const textColor = ["white", "red", "orange", "green", "pink", "yellow"];
+
+function createLi() {
+  for (i = 0; i < 100; i++) {
+    const liElement = document.createElement("li");
+    const textElement = document.createTextNode("blablablablabla");
+    liElement.appendChild(textElement);
+    list.appendChild(liElement);
+    const selectElement = document.createElement("select");
+    list.appendChild(selectElement);
+    for (const color of textColor) {
+      const optSelect = document.createElement("option");
+      const optColor = document.createTextNode(color);
+      optSelect.appendChild(optColor);
+      selectElement.appendChild(optSelect);
+    }
+  }
+}
+
+function changeColor(ev) {
+  const selectValue = ev.target.value;
+  const parentSelect = selectValue.parentElement;
+  console.log(ev.target);
+  console.log(parentSelect);
+}
+
+createLi();
+const selectAll = document.querySelectorAll("select");
+for (const select of selectAll) {
+  select.addEventListener("change", changeColor);
+}
